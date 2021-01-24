ea5ed34b9f959cc11430bdeab02692879301802e
Aleksey Tsvetkov <code.aleksey@icloud.com>
2021-01-24 13:23:54 +0300

    updated deps

--- apps/castmate-api/src/main.ts
+++ apps/castmate-api/src/main.ts
@@ -10,12 +10,9 @@ async function bootstrap() {
     },
   });
 
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+    Logger.log('Listening at http://localhost:' + port);
   });
 }
 
