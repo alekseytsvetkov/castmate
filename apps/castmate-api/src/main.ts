9c59b8d449e5abd8060a926634a68e52509ae62d
Aleksey Tsvetkov <code.aleksey@icloud.com>
2021-02-07 03:03:19 +0300

    updated sentry

--- apps/castmate-api/src/main.ts
+++ apps/castmate-api/src/main.ts
@@ -4,7 +4,6 @@ import * as session from 'express-session';
 import { SocketIoAdapter } from './app/adapters/socket-io.adapter';
 import { AppModule } from './app/app.module';
 import * as Sentry from "@sentry/node";
-import * as Tracing from "@sentry/tracing";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -14,10 +13,15 @@ async function bootstrap() {
     },
   });
 
-
   Sentry.init({
     dsn: "https://01c6d7a9b96541f4bf06299265be3b2f@o486817.ingest.sentry.io/5626220",
+    environment: process.env.ENVIRONMENT,
+    release: process.env.RELEASE,
 
+    integrations: [
+      // enable HTTP calls tracing
+      new Sentry.Integrations.Http({ tracing: true }),
+    ],
     // We recommend adjusting this value in production, or using tracesSampler
     // for finer control
     tracesSampleRate: 1.0,
