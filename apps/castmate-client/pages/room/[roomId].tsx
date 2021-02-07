716a79fe4873a1d430fa8c6425bcd91084c5f3af
Aleksey Tsvetkov <code.aleksey@icloud.com>
2021-02-07 23:15:05 +0300

    join a room from the room page

--- apps/castmate-client/pages/room/[roomId].tsx
+++ apps/castmate-client/pages/room/[roomId].tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CurrentRoom } from '@castmate/room';
+import { CurrentRoom, useJoinRoomMutation } from '@castmate/room';
 import { CastmateLayout } from '@castmate/ui';
 import { Community } from '@castmate/community';
 import { useRouter } from 'next/router';
@@ -19,6 +19,22 @@ export function RoomPage() {
   const { data, loading } = useRoomQuery({
     variables: { roomId },
   });
+
+  const [joinRoomMutation, { data: dataJoinRoom, loading: loadingJoinRoom, error: errorJoinRoom }] = useJoinRoomMutation();
+
+  const joinRoom = async (roomId: string) => {
+    const response = await joinRoomMutation({
+      variables: {
+        input: {
+          roomId
+        }
+      },
+    })
+
+    console.log('response', response);
+  }
+
+  joinRoom(roomId);
 
   // const loading = true;
 
